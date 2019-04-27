d322e806381baf1057db32dc3d147089504c6188
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2019-04-28 02:06:54 +0300

    Add component product, scalar product and normalization of vectors

--- goengine.go
+++ goengine.go
@@ -1,9 +1,11 @@
 package goengine
 
+import "math"
+
 //Vector struct represents geometric element of vector space
 type Vector struct {
-	X int64
-	Y int64
+	X float64
+	Y float64
 }
 
 //Add function initializes new Vector instance, that represents sum of origin and added vectors
@@ -14,4 +16,22 @@ func (v *Vector) Add(addedVector Vector) Vector {
 //Subtract function initializes new Vector instance, that represents subtruction of subtracted vector from origin vector
 func (v *Vector) Subtract(subtractedVector Vector) Vector {
 	return Vector{X: v.X - subtractedVector.X, Y: v.Y - subtractedVector.Y}
+}
+
+//ComponentProduct function initializes new Vector, that represents component product calculated by multiplying component of each vector by component of other vector
+func (v *Vector) ComponentProduct(vector Vector) Vector {
+	return Vector{X: v.X * vector.X, Y: v.Y * vector.Y}
+}
+
+//ScalarProduct function returns value, that represent scalar product both vectors A and B (it can be given by formula Ax*Bx+Ay*By or |A|*|B|*cos(alfa))
+func (v *Vector) ScalarProduct(vector Vector) float64 {
+	return vector.X*v.X + vector.Y*v.Y
+}
+
+//Normalize function returns new Vector instance that represents normalized vector, given by formula N = [Ax / length, Ay / length]; where length = sqrt(Ax^2 + Xy^2)
+func (v *Vector) Normalize() Vector {
+	vectorLength := math.Sqrt(v.X*v.X + v.Y*v.Y)
+	normalizedX := v.X / vectorLength
+	normalizedY := v.Y / vectorLength
+	return Vector{X: normalizedX, Y: normalizedY}
 }
