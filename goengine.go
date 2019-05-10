3b6853f9bdb080af99707de7fbc889e4817898e8
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2019-05-11 02:47:18 +0300

    add primitive and simple particle simulation without deep testing

--- goengine.go
+++ goengine.go
@@ -1,6 +1,10 @@
 package goengine
 
-import "math"
+import (
+	"math"
+)
+
+/*vector section*/
 
 //Vector struct represents geometric element of vector space
 type Vector struct {
@@ -38,8 +42,54 @@ func (v *Vector) Normalize() Vector {
 	return Vector{X: normalizedX, Y: normalizedY, Z: normalizedZ}
 }
 
+//Scale function returns new Vector that represents scaled vector based on origin vector, given by formula S = [x * number, y * number, z * number]
+func (v *Vector) Scale(number float64) Vector {
+	return Vector{X: v.X * number, Y: v.Y * number, Z: v.Z * number}
+}
+
 //VectorProduct function returns new Vector instance, that represents result of vector product of vectors, given by formula
 //VP(A, B) = [Ay*Bz - Az*By, Az*Bx - Ax*Bz, Ax*By - Ay*Bx]
 func (v *Vector) VectorProduct(vector Vector) Vector {
 	return Vector{X: v.Y*vector.Z - v.Z*vector.X, Y: v.Z*vector.X - v.X*vector.Z, Z: v.X*vector.Y - v.Y*vector.X}
+}
+
+/*end vector section*/
+
+//Particle struct represents simplest unit of physic space without direction
+type Particle struct {
+	Position       Vector
+	LinearVelocity Vector //linearVelocity
+	Acceleration   Vector
+}
+
+//Integrate function calculate next state of particle, including position and linear velocity
+func (particle *Particle) Integrate(duration float64) {
+	particle.Position = particle.Position.Add(particle.LinearVelocity.Scale(duration))
+	particle.LinearVelocity = particle.LinearVelocity.Add(particle.Acceleration.Scale(duration))
+}
+
+//SetAcceleration function sets acceleration vector for particle
+func (particle *Particle) SetAcceleration(acc Vector) {
+	particle.Acceleration = acc
+}
+
+//World struct represents entire game world with its particles
+type World struct {
+	particles []*Particle
+}
+
+//AddParticle function adds new particle into world
+func (world *World) AddParticle(position Vector) {
+	particle := &Particle{}
+	particle.Position = position
+	particle.Acceleration = Vector{X: 0, Y: 0, Z: 0}
+	particle.LinearVelocity = Vector{X: 0, Y: 0, Z: 0}
+	world.particles = append(world.particles, particle)
+}
+
+//DoStep function does processing of game world and all its objects
+func (world *World) DoStep(duration float64) {
+	for _, particle := range world.particles {
+		particle.Integrate(duration)
+	}
 }
--- goengine_test.go
+++ goengine_test.go
@@ -1,6 +1,9 @@
 package goengine
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 //TestSum test represents example of unit test for my go physics engine. In this case test verifies result of sum of two vectors
 func TestSum(t *testing.T) {
@@ -8,9 +11,19 @@ func TestSum(t *testing.T) {
 	var secondVector = Vector{X: 2, Y: 3}
 	sumVector := firstVector.Add(secondVector)
 	if sumVector.X != 5 {
-		t.Errorf("X was incorrect, got: %d, want: %d.", sumVector.X, 5)
+		t.Errorf("X was incorrect, got: %f, want: %d.", sumVector.X, 5)
 	}
 	if sumVector.Y != 8 {
-		t.Errorf("Y incorrect, got: %d, want %d.", sumVector.Y, 8)
+		t.Errorf("Y incorrect, got: %f, want %d.", sumVector.Y, 8)
 	}
+}
+
+func TestRunEngine(t *testing.T) {
+	world := World{}
+	world.AddParticle(Vector{X: 1, Y: 1, Z: 1})
+	world.particles[0].SetAcceleration(Vector{0.5, 0.33, 5.0})
+	for i := 0; i < 100; i++ {
+		world.DoStep(0.1)
+	}
+	fmt.Println("Engine worked out")
 }
